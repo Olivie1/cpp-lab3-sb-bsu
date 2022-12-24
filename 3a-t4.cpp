4c7698fa2935b911a9203abbee678ffef4a42005
Karina <55213206+Olivie1@users.noreply.github.com>
2022-12-24 07:11:23 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -5,11 +5,15 @@ int main()
 {
 	int k, i = 1, sum_k = 0, number;
 	cout << " Enter the amount of integers to sum = "; cin >> k;
-	do {
-		cout << "Enter integer nr. " << i << ": ";
-		cin >> number;
-		sum_k += number; i++;
-	} while (i <= k);
-	cout << " The total sum of " << k << " integers is: " << sum_k;
+	if (k == 0) 
+		cout << "You have 0 integers to count"; else {
+		do {
+			cout << "Enter integer nr. " << i << ": ";
+			cin >> number;
+			sum_k += number; i++;
+		} while (i <= k);
+		cout << " The total sum of " << k << " integers is: " << sum_k;
+	}
+	
 	return 0;
 }
