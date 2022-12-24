4de674df0ef6773429d453daf023cdd260e2ff0f
Karina <55213206+Olivie1@users.noreply.github.com>
2022-12-24 07:17:39 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,19 +1,35 @@
-#include<iostream> 
+#include<iostream>
+#include<limits>
 using namespace std;
 
 int main()
 {
-	int k, i = 1, sum_k = 0, number;
-	cout << " Enter the amount of integers to sum = "; cin >> k;
-	if (k == 0) 
-		cout << "You have 0 integers to count"; else {
-		do {
-			cout << "Enter integer nr. " << i << ": ";
-			cin >> number;
-			sum_k += number; i++;
-		} while (i <= k);
-		cout << " The total sum of " << k << " integers is: " << sum_k;
-	}
-	
-	return 0;
+    int k, i = 1, sum_k = 0, number;
+
+    cout << " Enter the amount of integers to sum = "; cin >> k;
+
+    cin >> k;
+    while (true)
+    {
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid number!" << endl;
+            cin >> k;
+        } else
+            break;
+    }
+    if (k == 0) cout << "You have 0 integers to count";
+    else
+
+        do
+        {
+            cout << "Enter integer nr. " << i << ": ";
+            cin >> number;
+            sum_k += number; i++;
+        } while (i <= k);
+        cout << " The total sum of " << k << " integers is: " << sum_k;
+        return 0;
+
 }
